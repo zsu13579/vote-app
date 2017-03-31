418d4cff87e39ae0e47d8c42130399a3636acd1d
jacklv <jacklvabcd@gmail.com>
2017-03-31 08:52:46 +0800

    try

--- app/routes/index.js
+++ app/routes/index.js
@@ -21,6 +21,7 @@ module.exports = function (app, db) {
 		var headers = req.headers;
 		var path = "/login/oauth/access_token";
 		headers.host = 'github.com';
+		headers['User-Agent']='vote-app';
 
 		path += '?client_id=' + '26f36d99ba1bcd3fbcc7';
 		path += '&client_secret='+'de201129efddddc3827c0d5483b85641f6df87a3';
@@ -33,27 +34,50 @@ module.exports = function (app, db) {
 			headers:headers,
 			method:'POST'
 		};
-		// var req1 = https.request(opts, function(res){
-		// 	res.setEncoding('utf8');
-		// 	res.on('data', function(data){
-		// 		var args = data.split('&');
-		// 		var tokenInfo = args[0].split("=");
-		// 		var token = tokenInfo[1];
-  //       // res1.send(token)
-        
-		// 	})
+		var req1 = https.request(opts, function(res){
+			res.setEncoding('utf8');
+			res.on('data', function(data){
+				var args = data.split('&');
+				var tokenInfo = args[0].split("=");
+				var token = tokenInfo[1];
+				  var path2="/user?access_token="+token;
+				  // res1.send(JSON.stringify(token));
+				  var headers={'User-Agent': 'vote-app'};
+				  // var url = "https://api.github.com/user?access_token="+token;
+				  var req2 = https.request({host:'api.github.com',port:'443',path:path2,method:'GET',headers:headers},function(res2){
+				  res2.setEncoding('utf8');
+				  res2.on('data',function(data){
+					// TODO					
+					console.log(data);
+				  })
+				});
+				req2.end(); 
+				// TODO when to redirect
+				res1.redirect("/newpolls");
+				// https.get(url,function(res){
+					// // res.setEncoding('utf8');					
+					// res.on('data', function(data){
+						// console.log(data);
+						// res1.render("pages/index");
+						
+					// });				
+				// })
+				
+			})
 			
-  //       });
-		// req1.end();
-    headers.host = 'api.github.com';
-    var path2= "/user?access_token="+"367519db02107a19bea255b63ae0878ce9705049"
-        var req2 = https.request({host:'api.github.com',port:'443',path:path2,headers:headers,method:'GET',},function(res2){
-          res2.setEncoding('utf8');
-          res2.on('data',function(data){
-            res1.send(JSON.stringify(data))
-          })
         });
-        req2.end(); 
+		req1.end();
+    // headers.host = 'api.github.com';
+	// headers.Authorization = 'token '+"367519db02107a19bea255b63ae0878ce9705049"
+    // var path2= "/user"
+        // var req2 = https.request({host:'api.github.com',port:'443',path:path2,headers:headers,method:'GET',},function(res2){
+          // res2.setEncoding('utf8');
+          // res2.on('data',function(data){
+            // // res1.send(JSON.stringify(data));
+			// console.log(JSON.stringify(data));
+          // })
+        // });
+        // req2.end(); 
 		 
       });	  
 
